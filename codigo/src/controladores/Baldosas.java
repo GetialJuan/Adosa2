f24d70623b0ae4abdc089aa7bcad3ab100dba012
GetialJuan <getial.juan@correounivalle.edu.co>
2022-06-30 21:29:57 -0500

    Se eliminaron 2 imagenes de baldosas

--- codigo/src/controladores/Baldosas.java
+++ codigo/src/controladores/Baldosas.java
@@ -27,7 +27,7 @@ public class Baldosas {
         ImageIcon baldosa;
         String rutaAux = "\\src\\imagenes\\baldosas\\numero.png";
 
-        for (int i = 1; i <= 16; i++) {
+        for (int i = 1; i <= 14; i++) {
             
             String nuevaRuta = rutaAux.replace("numero",i+"");
             
